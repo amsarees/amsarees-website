1a6c76df27d378ffa2674b582b57c9de7214b086
amsarees <a.m.sareescentre@gmail.com>
2026-05-02 19:36:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const app = express();
 
 app.use(express.json());
 
-// VERY IMPORTANT (serve all files)
-app.use(express.static(path.join(__dirname)));
-
-// login details
+// LOGIN DETAILS
 const USERNAME = "amsareescentre";
 const PASSWORD = "amsarees786@";
 
-// login API
+// SHOW admin.html on HOME
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "admin.html"));
+});
+
+// LOGIN API
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
@@ -23,11 +25,5 @@ app.post("/login", (req, res) => {
   }
 });
 
-// homepage (ONLY if root)
-app.get("/", (req, res) => {
-  res.send("AMSAREES WEBSITE WORKING ✅");
-});
-
-// start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running"));
